4487e4c3e6a4783330b8042a2944957d14f7c37e
Administrator <support@directvoip.co.uk>
2017-01-21 20:22:04 +0000

    Slight tweaks

--- debian/install.sh
+++ debian/install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-########## Edit these values to suit ##########
+########## Edit these values to suit and copy to second machine. Remember to set second machine to 'slave' ##########
 export master_ip='192.168.1.116'
-export slave_ip='192.168.1.117'
+export slave_ip='192.168.1.110'
 # node_type must be either 'master' or 'slave'
 export node_type='master'
 #Fill in the database password on each node. Please change the one below to the one generated in the instructions.
 export database_password='4iaUT6sqPKSeRAMiPItELbBpBS8'
-###############################################
+#####################################################################################################################
 
 #move to script directory so all relative paths work
 cd "$(dirname "$0")"
