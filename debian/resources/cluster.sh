fa94316af2f73c635f9b7eaabfb04b01fb5aabe9
root <root@debian.local>
2017-01-22 15:18:24 +0000

    Restore IP ro previous settings

--- debian/resources/cluster.sh
+++
@@ -1,7 +0,0 @@
-export master_ip='192.168.1.110'
-export slave_ip='192.168.1.86'
-export node_type='master'
-export database_password='zxZQNrQpGFRAqprn0sjesMIFj4'
-export xml_cdr_username='0NkSFzBpnwV05cGM'
-export xml_cdr_password='YlCvmkE7Mfsyy680'
-
