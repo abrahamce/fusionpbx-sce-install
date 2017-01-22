3a7804e8bd9dab46e4fd6ea1c61ccc41e5283de4
root <root@debian.local>
2017-01-22 15:13:51 +0000

    Fix bug with carriage returns in vars

--- debian/resources/iptables.sh
+++ debian/resources/iptables.sh
@@ -1,16 +1,14 @@
-#!/bin/sh
-
+#!/bin/bash
 #includes
 . ./resources/cluster.sh
-
 #send a message
 echo "Configure IPTables"
 
 #run iptables commands
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -I INPUT -s $master_ip -j ACCEPT
-iptables -I INPUT -s $slave_ip -j ACCEPT
+su -c "iptables -A INPUT --source $master_ip --jump ACCEPT"
+iptables -A INPUT -s "${slave_ip}" -j ACCEPT
 iptables -A INPUT -j DROP -p udp --dport 5060:5061 -m string --string "friendly-scanner" --algo bm
 iptables -A INPUT -j DROP -p udp --dport 5060:5061 -m string --string "sipcli/" --algo bm
 iptables -A INPUT -j DROP -p udp --dport 5060:5061 -m string --string "VaxSIPUserAgent/" --algo bm
@@ -41,3 +39,32 @@ iptables -P OUTPUT ACCEPT
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections
 apt-get install -y --force-yes  iptables-persistent
+cat <<EOF > /etc/iptables/rules.v4
+-A INPUT -i lo -j ACCEPT
+-A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+-A INPUT --source ${master_ip} -J ACCEPT
+-A INPUT -j DROP -p udp --dport 5060:5061 -m string --string "friendly-scanner" --algo bm
+-A INPUT -j DROP -p udp --dport 5060:5061 -m string --string "sipcli/" --algo bm
+-A INPUT -j DROP -p udp --dport 5060:5061 -m string --string "VaxSIPUserAgent/" --algo bm
+-A INPUT -j DROP -p tcp --dport 5060:5061 -m string --string "friendly-scanner" --algo bm
+-A INPUT -j DROP -p tcp --dport 5060:5061 -m string --string "sipcli/" --algo bm
+-A INPUT -j DROP -p tcp --dport 5060:5061 -m string --string "VaxSIPUserAgent/" --algo bm
+-A INPUT -j DROP -p udp --dport 5080:5081 -m string --string "friendly-scanner" --algo bm
+-A INPUT -j DROP -p udp --dport 5080:5081 -m string --string "sipcli/" --algo bm
+-A INPUT -j DROP -p udp --dport 5080:5081 -m string --string "VaxSIPUserAgent/" --algo bm
+-A INPUT -j DROP -p tcp --dport 5080:5081 -m string --string "friendly-scanner" --algo bm
+-A INPUT -j DROP -p tcp --dport 5080:5081 -m string --string "sipcli/" --algo bm
+-A INPUT -j DROP -p tcp --dport 5080:5081 -m string --string "VaxSIPUserAgent/" --algo bm
+-A INPUT -p tcp --dport 22 -j ACCEPT
+-A INPUT -p tcp --dport 80 -j ACCEPT
+-A INPUT -p tcp --dport 443 -j ACCEPT
+-A INPUT -p tcp --dport 5060:5061 -j ACCEPT
+-A INPUT -p udp --dport 5060:5061 -j ACCEPT
+-A INPUT -p tcp --dport 5080:5081 -j ACCEPT
+-A INPUT -p udp --dport 5080:5081 -j ACCEPT
+-A INPUT -p udp --dport 16384:32768 -j ACCEPT
+-A INPUT -p icmp --icmp-type echo-request -j ACCEPT
+-A INPUT -p udp --dport 1194 -j ACCEPT
+-P INPUT DROP
+-P FORWARD DROP
+-P OUTPUT ACCEPT
