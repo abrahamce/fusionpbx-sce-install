ddfac165c4ef6bdba31b5fa0fbba8e6e0a9ca330
Administrator <admin@example.com>
2017-01-23 00:57:08 +0000

    Update fusionpbx.sh add modules.php

--- debian/resources/fusionpbx.sh
+++ debian/resources/fusionpbx.sh
@@ -32,6 +32,7 @@ git clone $BRANCH https://github.com/fusionpbx/fusionpbx.git /var/www/fusionpbx
 cp /usr/src/fusionpbx-sce-install/debian/resources/ha_monitor.lua /var/www/fusionpbx/resources/install/scripts
 cp /usr/src/fusionpbx-sce-install/debian/resources/lua.conf.xml /var/www/fusionpbx/resources/templates/conf/autoload_configs/lua.conf.xml
 cp /usr/src/fusionpbx-sce-install/debian/resources/modules.conf.xml /var/www/fusionpbx/resources/templates/conf/autoload_configs/modules.conf.xml
+cp /usr/src/fusionpbx-sce-install/debian/resources/modules.php /var/www/fusionpbx/app/modules/resources/classes/modules.php
 sed -i /var/www/fusionpbx/resources/install/scripts/ha_monitor.lua -e s:"rpc_user:$xml_cdr_username:"
 sed -i /var/www/fusionpbx/resources/install/scripts/ha_monitor.lua -e s:"rpc_pass:$xml_cdr_password:"
 if [ $node_type = 'master' ]; then
